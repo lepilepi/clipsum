3d22e6ab90fb38d01babef2a6be334ee509f6ac0
lepi <lepi@symbler.com>
2011-11-24 06:31:50 +0100

    surf test

--- surf_test.py
+++ surf_test.py
@@ -3,24 +3,45 @@ from cv import CV_LOAD_IMAGE_COLOR as COLOR
 from cv import CV_LOAD_IMAGE_GRAYSCALE as GREYSCALE
 from cv import CV_BGR2GRAY as BGR2GRAY
 from cv import CV_GRAY2BGR as GRAY2BGR
+from cv import CV_CAP_PROP_POS_MSEC as MSEC_POS
+from cv import CV_CAP_PROP_POS_FRAMES as FRAME_POS
 from cv import CV_8U,CV_8UC3, WaitKey, ConvertScale,Merge,SaveImage
 import cv,sys
 
-#"result/fut_full5_shots/f.97358.jpg"
-im_grayscale = LoadImageM(sys.argv[1], GREYSCALE)
-im_color =  CreateMat(im_grayscale.height,  im_grayscale.width,  CV_8UC3)
+print """usage:
+    cilpsum.py image [list_only|show|save] <path-to-image>
+    cilpsum.py video [list_only|show|save] <path-to-video> [M<pos-in-msec>|F<framnumber>]
+        """
 
-for arg in sys.argv[1:]:
+if sys.argv[1]=='video':
+    capture = cv.CaptureFromFile(sys.argv[3])
+    if sys.argv[4][0]=='M':
+        cv.SetCaptureProperty(capture, MSEC_POS, int(sys.argv[4][1:]))
+    elif sys.argv[4][0]=='F':
+        cv.SetCaptureProperty(capture, FRAME_POS, int(sys.argv[4][1:]))
+    img = cv.QueryFrame(capture)
+    im_grayscale = CreateMat(img.height,  img.width,  CV_8U)
+    CvtColor(img,im_grayscale,BGR2GRAY);
+elif sys.argv[1]=='image':
+    im_grayscale = LoadImageM(sys.argv[3], GREYSCALE)
+else:
+    sys.exit(-1)
 
-    im_grayscale = LoadImageM(arg, GREYSCALE)
-    (keypoints, descriptors) = ExtractSURF(im_grayscale, None, CreateMemStorage(), (0, 30000, 3, 1))
-    Merge( im_grayscale, im_grayscale, im_grayscale, None, im_color )
-    print len(keypoints), len(descriptors)
-    for ((x, y), laplacian, size, dir, hessian) in keypoints:
-        print "x=%d y=%d laplacian=%d size=%d dir=%f hessian=%f" % (x, y, laplacian, size, dir, hessian)
-        Circle(im_color,(x,y),size,Scalar(0,255,0),1)
+if sys.argv[2] in ['list_only','save','show']:
+    im_color =  CreateMat(im_grayscale.height,  im_grayscale.width,  CV_8UC3)
 
-    SaveImage('.'.join(arg.split('.')[:-1]) + '.SURF.jpg', im_color)
+    for arg in sys.argv[3:]:
 
-#ShowImage("SURF test",im_color)
-#while True:pass
+        (keypoints, descriptors) = ExtractSURF(im_grayscale, None, CreateMemStorage(), (1, 20000, 3, 1))
+        Merge( im_grayscale, im_grayscale, im_grayscale, None, im_color )
+        print len(keypoints), len(descriptors)
+        for ((x, y), laplacian, size, dir, hessian) in keypoints:
+            print "x=%d y=%d laplacian=%d size=%d dir=%f hessian=%f" % (x, y, laplacian, size, dir, hessian)
+            Circle(im_color,(x,y),size,Scalar(0,255,0),1)
+
+        if sys.argv[2]=="save":
+            SaveImage('.'.join(arg.split('.')[:-1]) + '.SURF.%s.jpg' % sys.argv[4][1:], im_color)
+
+        if sys.argv[2]=="show":
+            ShowImage("SURF test",im_color)
+            input()
