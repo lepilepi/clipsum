c24b647479e00995e64ed172b1ad520014227e30
Tamas Leposa <lt@lab85.com>
2012-11-28 19:10:32 +0100

    converting SURF database (sqlite) to HDF file, but stores keypoint informations too, not only descriptors

--- sqlite2hdf.py
+++ sqlite2hdf.py
@@ -19,28 +19,73 @@ if __name__ == '__main__':
 
     n_keypoints = db.c.execute("SELECT COUNT(*) FROM keypoints").fetchone()[0]
     print n_keypoints
-    m=np.empty((n_keypoints,128) ,dtype=np.float32)
+    m=np.empty((n_keypoints,137) ,dtype=np.float32)
 
     print "Starting reading database file and building numpy array..."
     d1 = datetime.now()
     # -------------------------------
-    for i, keyp in enumerate(db.c.execute("SELECT * FROM keypoints")):
-        m[i]= keyp[7:]
+    for i, keyp in enumerate(db.c.execute("SELECT * FROM frames INNER JOIN keypoints ON frames.id=keypoints.frame_id")):
+#        m[i]= keyp[7:]
+        m[i]= keyp[:]
     # -------------------------------
     d2 = datetime.now()
     print "Read complete, elapsed time: %d.%d\n" % ((d2-d1).seconds, (d2-d1).microseconds)
 
-
     print "Saving as DHF file..."
-    d1 = datetime.now()
-    #    ****************************
+    # *******************************
+    # Creating CArray for descriptors
+    # *******************************
     f = tables.openFile(filename, 'w')
     atom = tables.Atom.from_dtype(m.dtype)
     filters = tables.Filters(complib='blosc', complevel=1)
-    ds = f.createCArray(f.root, 'descriptors', atom, m.shape, filters=filters)
-    ds[:] = m
-    f.close()
-    #    ****************************
+    descriptors = f.createCArray(f.root, 'descriptors', atom, (len(m),128), filters=filters)
+
+    # *****************************
+    # Creating tables for keypoints
+    # *****************************
+    schema = {
+        'pos':      tables.IntCol(),
+        'x':        tables.FloatCol(),
+        'y':        tables.FloatCol(),
+        'laplacian':tables.IntCol(),
+        'size':     tables.IntCol(),
+        'dir':      tables.FloatCol(),
+        'hessian':  tables.FloatCol(),
+        }
+    f.createTable('/', 'keypoints', schema)
+    keypoints=f.root.keypoints
+    # *******************************************************
+    # Fill up keypoints table and descriptors array with data
+    # *******************************************************
+
+    d1 = datetime.now()
+    print "Bulilding keypoints..."
+
+    for kp in m[:,:9]:
+        r = keypoints.row
+
+        r['pos'] = kp[1]
+        r['x'] = kp[3]
+        r['y'] = kp[4]
+        r['laplacian'] = kp[5]
+        r['size'] = kp[6]
+        r['dir'] = kp[7]
+        r['hessian'] = kp[8]
+        r.append()
+
+    keypoints.flush()
 
     d2 = datetime.now()
-    print "Done. Elapsed time: %d.%d" % ((d2-d1).seconds, (d2-d1).microseconds)
+    print "Table saved. Elapsed time: %d.%d" % ((d2-d1).seconds, (d2-d1).microseconds)
+
+    d1 = datetime.now()
+
+    print "Bulilding descriptors..."
+
+    descriptors[:] = m[:,9:]
+    f.close()
+    # *********************************************************
+
+
+    d2 = datetime.now()
+    print "Descriptors saved. Elapsed time: %d.%d" % ((d2-d1).seconds, (d2-d1).microseconds)
