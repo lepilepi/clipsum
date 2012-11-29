34aea0fe135505a366eb7f37162358ad205ff2fb
Tamas Leposa <lt@lab85.com>
2012-11-29 03:25:12 +0100

    updated surf_test

--- surf_test.py
+++ surf_test.py
@@ -9,8 +9,8 @@ from cv import CV_8U,CV_8UC3, WaitKey, ConvertScale,Merge,SaveImage
 import cv,sys
 
 print """usage:
-    cilpsum.py image [list_only|show|save] <path-to-image>
-    cilpsum.py video [list_only|show|save] <path-to-video> [M<pos-in-msec>|F<framnumber>]
+    cilpsum.py image [list|show|save] <path-to-image>
+    cilpsum.py video [list|show|save] <path-to-video> [M<pos-in-msec>|F<framnumber>]
         """
 
 if sys.argv[1]=='video':
@@ -19,24 +19,22 @@ if sys.argv[1]=='video':
         cv.SetCaptureProperty(capture, MSEC_POS, int(sys.argv[4][1:]))
     elif sys.argv[4][0]=='F':
         cv.SetCaptureProperty(capture, FRAME_POS, int(sys.argv[4][1:]))
-    img = cv.QueryFrame(capture)
-    im_grayscale = CreateMat(img.height,  img.width,  CV_8U)
-    CvtColor(img,im_grayscale,BGR2GRAY);
+    im_color = cv.QueryFrame(capture)
+    im_grayscale = CreateMat(im_color.height,  im_color.width,  CV_8U)
+    CvtColor(im_color,im_grayscale,BGR2GRAY);
 elif sys.argv[1]=='image':
+    im_color = LoadImageM(sys.argv[3], COLOR)
     im_grayscale = LoadImageM(sys.argv[3], GREYSCALE)
 else:
     sys.exit(-1)
 
-if sys.argv[2] in ['list_only','save','show']:
-    im_color =  CreateMat(im_grayscale.height,  im_grayscale.width,  CV_8UC3)
-
+if sys.argv[2] in ['list','save','show']:
     for arg in sys.argv[3:]:
 
         (keypoints, descriptors) = ExtractSURF(im_grayscale, None, CreateMemStorage(), (1, 30, 3, 4))
-        Merge( im_grayscale, im_grayscale, im_grayscale, None, im_color )
         for ((x, y), laplacian, size, dir, hessian) in keypoints:
             print "x=%d y=%d laplacian=%d size=%d dir=%f hessian=%f" % (x, y, laplacian, size, dir, hessian)
-            Circle(im_color,(x,y),size,Scalar(0,255,0),1)
+            Circle(im_color,(int(x),int(y)),size/5,Scalar(0,255,0),1)
 
         print "Number of keypoinst: %d" % len(keypoints)
 
