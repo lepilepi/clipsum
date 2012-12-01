31c84406244dcc1506ea2130fd8ff08acef77062
Tamas Leposa <lt@lab85.com>
2012-12-01 20:49:34 +0100

    refactored query

--- query.py
+++ query.py
@@ -4,30 +4,32 @@ import sys
 import tables
 import numpy as np
 
+def query_region(filename, p, x1,y1, x2,y2):
+    filename = '%s.surf.hdf' % os.path.basename(filename)
 
-x1 = 100
-x2 = 300
-y1 = 100
-y2 = 300
-p = 5000
+    try:
+        f = tables.openFile(filename, 'r')
+    except IOError:
+        raise Exception("Please run 'surfdb.py' first!")
 
+    try:
+        f.root.clusters
+    except tables.NoSuchNodeError:
+        raise Exception("Please run 'cluster_descriptors.py' first!")
 
-filename = '%s.surf.hdf' % os.path.basename(sys.argv[1])
-f = tables.openFile(filename, 'r')
-
-
-try:
-    f.root.clusters
-except tables.NoSuchNodeError:
-    print "Please run 'cluster_descriptors.py' first!"
-    sys.exit(-1)
-
-feature_ids = f.root.keypoints.readWhere(
+    feature_ids = f.root.keypoints.readWhere(
             '(pos==%d) & (x>%d) & (x<%d) & (y>%d) & (y<%d)' % (p,x1,x2,y1,y2),
             field='cluster')
-frame_positions=[getattr(f.root.clusters, 'cluster_%d' % i)[0] for i in feature_ids]
+    frame_positions=[getattr(f.root.clusters, 'cluster_%d' % i)[0] for i in feature_ids]
 
-frame_positions=np.concatenate(frame_positions)
+    frame_positions=np.concatenate(frame_positions)
 
-cc=Counter(frame_positions)
-import pdb;pdb.set_trace()
+    cc=Counter(frame_positions)
+
+    return cc
+
+if __name__ == "__main__":
+    result = query_region(sys.argv[1], int(sys.argv[2]),
+                                            int(sys.argv[3]),int(sys.argv[4]),
+                                            int(sys.argv[5]),int(sys.argv[6]))
+    import pdb;pdb.set_trace()
